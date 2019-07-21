558571d8d01f9d8327cf60a46bf039902a6b88ae
johannesMatevosyan <h.matevosyan@yahoo.com>
2019-07-21 18:08:34 +0400

    redesign rx-options, changed code-mirror theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'RXbuilder';
+  title = 'RXBuilder';
   groups = [];
   chain = [];
   parentId ?: string = null;
